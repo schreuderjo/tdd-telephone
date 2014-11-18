9b64d6ef8865cf1d7bb471d438c59e1a134e97df
Gina Krieb & Jordan Schreuder <pair+rockrgrrl.schreuderjo@devbootcamp.com>
2014-11-18 13:38:55 -0600

    initial to-do list solution

--- todo-list/todo_spec.rb
+++ todo-list/todo_spec.rb
@@ -1,6 +1,6 @@
-require_relative 'todo.rb'
+require_relative 'todo'
 
-describe List do
+describe @List do
   before :each do
     # Create an array of 5 items here and name it @items
     # - the InventoryItems should have the names of 'shoe', 'shirt', 'glove', 'hat', and 'sock', in that order
@@ -18,75 +18,76 @@ describe List do
 
   describe '#initialize' do
     it 'should have a title' do
-      expect(list.title).to eq('grocery_list')
+      expect(@list.title).to eq('grocery_list')
     end
 
     it 'should have many tasks (it should be an array)' do
-      expect(list.tasks).to be_kind_of Array
+      expect(@list.tasks).to be_kind_of Array
     end
   end
 
   describe '#add_task' do
     it 'should add a task' do
-      list.add_task(task3)
-      expect(list.tasks.length).to eq(3)
+      @list.add_task(@task3)
+      expect(@list.tasks.length).to eq(3)
     end
 
     it 'should add a task object to the tasks array' do
-      list.add_task(task3)
-      expect(list.tasks.last).to be_kind_of Task
+      @list.add_task(@task3)
+      expect(@list.tasks.last).to be_kind_of Task
     end
   end
 
   describe '#complete_all!' do
     it 'should mark all tasks as complete' do
-      list.complete_all
+      @list.complete_all
       status_array = []
-      list.tasks.each do |task|
+      @list.tasks.each do |task|
         status_array << task.status
       end
-      expect(status_array.uniq).to eq(["completed"])
+      expect(status_array.uniq).to eq(["complete"])
     end
   end
 
   describe '#complete?' do
     it 'should return true when all tasks are complete' do
-      list.complete_all
-      expect(list.complete?).to eq(true)
+      @list.complete_all
+      expect(@list.complete?).to eq(true)
     end
 
     it 'should return false when any task is incomplete' do
-      list.complete_all
-      list.first.status = "incomplete"
-      expect(list.complete?).to eq(false)
+      @list.complete_all
+      @list.tasks.first.status = "incomplete"
+      expect(@list.complete?).to eq(false)
     end
   end
 
   describe '#completed_tasks' do
     it 'should return an array' do
-      expect(list.completed_tasks).to be_kind_of Array
+      expect(@list.completed_tasks).to be_kind_of Array
     end
 
     it 'should return an array of task objects' do
-      expect(list.completed_tasks.first).to be_kind_of Task
+      @list.complete_all
+      expect(@list.completed_tasks.first).to be_kind_of Task
     end
 
     it 'should return an array of only completed tasks' do
-      list.complete_all
-      list.first.status = "incomplete"
+      @list.complete_all
+      @list.tasks.first.status = "incomplete"
       status_array = []
-      list.completed_tasks.each do |task|
+      @list.completed_tasks.each do |task|
         status_array << task.status
       end
-      expect(status_array.uniq).to eq(["completed"])
+      expect(status_array.uniq).to eq(["complete"])
     end
   end
 
   describe '#incomplete_tasks' do
     it 'should return an array of only incomplete tasks' do
-      list.first.status = "completed"
+      @list.tasks.first.status = "completed"
       status_array = []
-      list.incomplete_tasks.each do |task|
+      @list.incomplete_tasks.each do |task|
         status_array << task.status
       end
       expect(status_array.uniq).to eq(["incomplete"])
